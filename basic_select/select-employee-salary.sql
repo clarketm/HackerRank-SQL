17d29328b79f2e1399d845986f02e485e6011960
Travis Clarke <travis.m.clarke@gmail.com>
2019-02-12 16:30:45 -0800

    complete "basic_select" sql solutions

---
+++ basic_select/select-employee-salary.sql
@@ -0,0 +1,5 @@
+-- Employee Salaries
+-- https://www.hackerrank.com/challenges/salary-of-employees/problem
+
+select name from employee where salary > 2000 and months < 10 order by employee_id asc
+-- select name from employee where salary > 2000 and months < 10 order by employee_id asc asc
--- basic_select/station-5.sql
+++ basic_select/station-5.sql
@@ -1,4 +1,7 @@
 -- Weather Observation Station 5
 -- https://www.hackerrank.com/challenges/weather-observation-station-5/problem
 
--- TODO
+select city, length(city) from station order by length(city) asc, city limit 1;
+select city, length(city) from station order by length(city) desc, city limit 1
+-- select city, length(city) from station order by length(city) asc, city asc limit 1;
+-- select city, length(city) from station order by length(city) desc, city asc limit 1;
